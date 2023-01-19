39275df5ff92cdb290d1993dc1878475628bcc4f
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-01-19 20:51:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Responsive nav
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -12,3 +13,19 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+
+// Nav effect
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("mynav").style.height = "4rem";
+        document.getElementById("nav-logo").style.width = "45px";
+        document.getElementById("nav-logo").style.height = "45px";
+        document.getElementById("mynav").style.backgroundColor = "black";
+    } else {
+        document.getElementById("mynav").style.height = "6rem";
+        document.getElementById("nav-logo").style.width = "55px";
+        document.getElementById("nav-logo").style.height = "55px";
+        document.getElementById("mynav").style.backgroundColor = "transparent";
+    }
+}
